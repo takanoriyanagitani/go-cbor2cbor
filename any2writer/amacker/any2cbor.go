a401aee44e67b5b6c1cbf42c47d28e3e57a2a622
yanagitani <takanori.yanagitani@gmail.com>
2024-10-15 09:53:05 +0900

    feat: time serialization mode from env var

--- any2writer/amacker/any2cbor.go
+++ any2writer/amacker/any2cbor.go
@@ -2,10 +2,32 @@ package any2cbor
 
 import (
 	"io"
+	"os"
 
 	ac "github.com/fxamacker/cbor/v2"
 
 	a2w "github.com/takanoriyanagitani/go-cbor2cbor/any2writer"
+)
+
+func TimeModeFromString(timeMode string) ac.TimeMode {
+	switch timeMode {
+	case "TimeUnix":
+		return ac.TimeUnix
+	case "TimeUnixMicro":
+		return ac.TimeUnixMicro
+	case "TimeUnixDynamic":
+		return ac.TimeUnixDynamic
+	case "TimeRFC3339":
+		return ac.TimeRFC3339
+	case "TimeRFC3339Nano":
+		return ac.TimeRFC3339Nano
+	default:
+		return ac.TimeUnix
+	}
+}
+
+var TimeModeFromEnv ac.TimeMode = TimeModeFromString(
+	os.Getenv("ENV_CBOR_TIME_MODE"),
 )
 
 type AnyArrayToCbor struct {
@@ -17,7 +39,9 @@ func (a AnyArrayToCbor) Encode(arr []any) error {
 }
 
 func AnyArrayToCborNew(wtr io.Writer) func([]any) error {
-	em, err := ac.CanonicalEncOptions().EncMode()
+	var eopt ac.EncOptions = ac.CanonicalEncOptions()
+	eopt.Time = TimeModeFromEnv
+	em, err := eopt.EncMode()
 	var a2c AnyArrayToCbor
 	if nil == err {
 		a2c.Encoder = em.NewEncoder(wtr)
